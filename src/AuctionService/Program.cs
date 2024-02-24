8bbb46bbf5eee285844563d694bcbe3970607076
Shohruh Abduvohidov <shohruhred@inbox.ru>
2024-02-24 20:44:58 +0500

    Add data to return

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 //builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
